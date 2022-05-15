7797cf187ebd26f5b2b10a5735b25430e9a65f70
anyexinglu <1171009543@qq.com>
2022-05-15 16:11:41 +0800

    add JIT

--- articles/JS_Basic/promise/try.js
+++ articles/JS_Basic/promise/try.js
@@ -1,27 +1,41 @@
+setImmediate(function(){
+  console.log(1);
+},0);
+setTimeout(function(){
+  console.log(2);
+},0);
+new Promise(function(resolve){
+  console.log(3);
+  resolve();
+  console.log(4);
+}).then(function(){
+  console.log(5);
+});
+console.log(6);
+console.log(8);
+  
+// const process = require('process')
+// process.on('unhandledRejection', (reason, promise) => {
+//   console.log('Unhandled Rejection at:', 'reason:', reason)
+// })
 
+//   function delay(ms) {
+//     return new Promise(r => setTimeout(r, ms));
+//   }
   
-const process = require('process')
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled Rejection at:', 'reason:', reason)
-})
-
-  function delay(ms) {
-    return new Promise(r => setTimeout(r, ms));
-  }
+//   async function fn() {
+//     await delay(100);
+//     throw new Error("111");
+//   }
   
-  async function fn() {
-    await delay(100);
-    throw new Error("111");
-  }
+//   async function main() {
+//     try {
+//       const p1 = await fn();
+//       await delay(200);
+//     }
+//     catch(e) {
+//       console.warn(`caught inside main: ${e.message}`);
+//     }
+//   }
   
-  async function main() {
-    try {
-      const p1 = await fn();
-      await delay(200);
-    }
-    catch(e) {
-      console.warn(`caught inside main: ${e.message}`);
-    }
-  }
-  
-  main().catch(e => console.warn(`caught on main: ${e.message}`));
+//   main().catch(e => console.warn(`caught on main: ${e.message}`));
