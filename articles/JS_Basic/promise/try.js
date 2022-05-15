dffb9ea886b5205c3f106b09789a6a22ddb850d2
anyexinglu <1171009543@qq.com>
2022-05-15 16:45:09 +0800

    update csrf

--- articles/JS_Basic/promise/try.js
+++ articles/JS_Basic/promise/try.js
@@ -1,18 +1,24 @@
-setImmediate(function(){
-  console.log(1);
-},0);
-setTimeout(function(){
-  console.log(2);
-},0);
+console.log(1)
 new Promise(function(resolve){
-  console.log(3);
-  resolve();
-  console.log(4);
-}).then(function(){
-  console.log(5);
-});
-console.log(6);
-console.log(8);
+    console.log(2);
+    resolve(3);
+    console.log(4);
+}).then(res => {
+    console.log(res)
+    process.nextTick(() => {
+        console.log('in nextTick')
+    });
+    Promise.resolve(5).then(res => {
+        console.log(res)
+    })
+    setImmediate(() => {
+        console.log('in setImmediate');
+    });
+})
+setTimeout(() => {
+    console.log('in setTimeout')
+}, 0)
+console.log(6)
   
 // const process = require('process')
 // process.on('unhandledRejection', (reason, promise) => {
