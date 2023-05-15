873020e6b5f137e4b4a045572b1c8ae8983f3ed3
yanxingzhe <yanxingzhe@pinduoduo.com>
2023-05-15 11:46:37 +0800

    调整

--- articles/JS_Basic/try.js
+++ articles/JS_Basic/try.js
@@ -1,20 +1,38 @@
+// JS 实现一个带并发限制的异度调度器 Scheduler，保证同时运行的任务最多有两个。完善下面代码中的 Scheduler 类，使得以下程序能正确输出。
 
-const debounce = (fn, ms = 0) => {
-  let timeoutId;
-  return function(...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function() {
-      fn(...args)
-    }, ms);
-  };
+class Scheduler {
+  tasks = []
+  waitList = []
+  add(promiseMaker) {
+    const currentLen = this.tasks.length
+    let task = () => promiseMaker().finally(() => {
+      this.tasks.unshift(currentLen, 1)
+      this.waitList.length && this.tasks.push(this.waitList.shift()())
+    })
+    if (this.tasks.length <= 1) {
+      this.tasks.push(task())
+    } else {
+      this.waitList.push(task)
+    }
+  }
 }
 
-// 试验：
-let person = {
-  name: 'John',
-  age: 23,
-  getName: debounce(function() {
-    console.log(this, this.name); // globalThis undefined
-  }, 50)
-}
-person.getName();
+const timeout = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+
+const scheduler = new Scheduler();
+const addTask = (time, order) => {
+  scheduler.add(() => timeout(time).then(() => console.log(order)));
+};
+
+addTask(1000, "1");
+addTask(500, "2");
+addTask(300, "3");
+addTask(400, "4");
+// output：2 3 1 4
+// 一开始，1，2两个任务进入队列。
+// 500ms 时，2完成，输出2，任务3入队。
+// 800ms 时，3完成，输出3，任务4入队。
+// 1000ms 时，1完成，输出1。
